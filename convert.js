16a0c01c71b5a226d7b072cebbf3bacfea299f60
markyleangela <markyleangela@gmail.com>
2023-12-21 04:32:18 +0800

    Added Base N to Base N Conversion

--- convert.js
+++ convert.js
@@ -67,6 +67,8 @@ function hex_to_dec(input){
     return answer;
 }
 
+
+
 function dec_to_bin(input){
     parseInt(input, 10);
     var answer = "";
@@ -129,6 +131,34 @@ function dec_to_hex(input){
 }
 
 
+function bin_to_oct(input){
+    return dec_to_oct(bin_to_dec(input));
+}
+
+function bin_to_hex(input){
+    return dec_to_hex(bin_to_dec(input));
+}
+
+function oct_to_bin(input){
+    return dec_to_bin(oct_to_dec(input));
+}
+
+function oct_to_hex(input){
+    return dec_to_hex(oct_to_dec(input));
+}
+
+function hex_to_bin(input){
+    return dec_to_bin(hex_to_dec(input));
+}
+
+function hex_to_oct(input){
+    return dec_to_oct(hex_to_dec(input));
+}
+
+
+
+
+
 function myFunction(){
     var input = document.getElementById('input_area').value;
     
@@ -153,6 +183,25 @@ function myFunction(){
         case 'dec_to_hex':
             document.getElementById("answer_label").innerHTML = dec_to_hex(input)
             break;
+        case 'bin_to_oct':
+            document.getElementById("answer_label").innerHTML = bin_to_oct(input)
+            break;
+        case 'bin_to_hex':
+            document.getElementById("answer_label").innerHTML = bin_to_hex(input)
+            break;
+        case 'oct_to_bin':
+            document.getElementById("answer_label").innerHTML = oct_to_bin(input)
+            break;
+        case 'oct_to_hex':
+            document.getElementById("answer_label").innerHTML = oct_to_hex(input)
+            break;
+        case 'hex_to_bin':
+            document.getElementById("answer_label").innerHTML = hex_to_bin(input)
+            break;
+        case 'hex_to_oct':
+            document.getElementById("answer_label").innerHTML = hex_to_oct(input)
+            break;
+        
     }
 
 }
